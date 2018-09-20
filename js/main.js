e861105bd9d0beeb1f5404aa30293c3931ed691d
nabitapradhan <nabitapradhan45@gmail.com>
2018-09-20 15:49:35 +0530

    Commit 3

--- js/main.js
+++ js/main.js
@@ -1,2 +1,59 @@
 "use strict";
-console.log("Hello");
+
+
+/*var randArr=[];
+for(var i=0;i<10;i++)
+{
+	
+	randArr.push(Math.ceil(Math.random()*101));
+}
+console.log(randArr);*/
+
+var chars=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","@","{","[","|",".","?","#","$","%","^","&","*"];
+var pass=" ";
+var a=Math.floor(Math.random()*5);
+a +=12;
+for(var j=0; j<a ;j++)
+{
+	var index=Math.floor(Math.random()*chars.length);
+	if(Math.random()>0.5 && index<26)
+	{
+			pass += chars[index].toUpperCase();
+	}
+	else
+	{
+		pass +=chars[index];
+	}
+}
+console.log(pass);
+
+/*for(var i=0;i<5;i++)
+{
+	console.log("Nabita");
+
+	for(var j=0;j<5;j++)
+	{
+	console.log("Pradhan");
+	}
+}
+
+*/
+var myTable= document.getElementById("nestedTable");
+for(var i=0;i<10;i++)
+{
+	var row=document.createElement("tr");
+	myTable.append(row);
+	
+for(var j=0;j<10;j++)
+{
+	var col=document.createElement("td");
+	col.setAttribute("class","Tabledata");
+	row.append(col);
+
+
+}
+}
+
+
+
+
